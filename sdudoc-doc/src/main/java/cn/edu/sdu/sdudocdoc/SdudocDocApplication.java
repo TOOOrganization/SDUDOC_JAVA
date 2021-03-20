6eebb818e10a6a11d0730093ee3c7aa1c38a594a
iwscream <874223998>
2021-03-20 23:52:37 +0800

    first commit

---
+++ sdudoc-doc/src/main/java/cn/edu/sdu/sdudocdoc/SdudocDocApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.sdu.sdudocdoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdudocDocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SdudocDocApplication.class, args);
+    }
+
+}
