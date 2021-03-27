f2be2fa412d3756584b325dbe8ea70a52a2525e5
iwscream <874223998>
2021-03-27 21:57:33 +0800

    mybatis generator

---
+++ sdudoc-doc/src/main/java/cn/edu/sdu/sdudocdoc/controller/HelloController.java
@@ -0,0 +1,15 @@
+package cn.edu.sdu.sdudocdoc.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HelloController {
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String hello() {
+        
+        return "hello world!";
+    }
+}
