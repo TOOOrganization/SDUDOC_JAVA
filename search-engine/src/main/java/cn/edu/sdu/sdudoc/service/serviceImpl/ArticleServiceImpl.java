3fa3272e7ef20b187c7ebee9fef35d3c5a87017c
iwscream <874223998>
2021-03-30 12:52:33 +0800

    add module sdudoc-dao

---
+++ search-engine/src/main/java/cn/edu/sdu/sdudoc/service/serviceImpl/ArticleServiceImpl.java
@@ -0,0 +1,73 @@
+package cn.edu.sdu.sdudoc.service.serviceImpl;
+
+import cn.edu.sdu.sdudoc.entity.*;
+import cn.edu.sdu.sdudoc.repository.*;
+import cn.edu.sdu.sdudoc.service.ArticleService;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Component
+public class ArticleServiceImpl implements ArticleService {
+
+    @Resource
+    private ArticleRepository articleRepository;
+
+    @Override
+    public void save(Article article) {
+        articleRepository.save(article);
+    }
+
+    @Override
+    public List<Article> saveAll(List articles){
+        return articleRepository.saveAll(articles);
+    }
+
+    @Override
+    public void delete(String id) {
+        articleRepository.deleteById(id);
+    }
+
+//    @Override
+//    public void update(Article article) {
+//        Query query = new Query(Criteria.where("_id").is(article.getId()));
+//
+//        Update update = new Update();
+//        update.set("_id", article.getId());
+//        update.set("articleAuthor", article.getArticleAuthor());
+//        update.set("bookname", article.getBookName());
+//        update.set("content", article.getContent());
+//        update.set("imgurl", article.getImgUrl());
+//        update.set("page", article.getPage());
+//        update.set("title", article.getTitle());
+//        update.set("XML", article.getXml());
+//
+//        mongoTemplate.updateFirst(query, update, Article.class);
+//    }
+    @Override
+    public void update(Article article){
+        articleRepository.save(article);
+    }
+
+    @Override
+    public List<Article> findAll() {
+        return articleRepository.findAll();
+    }
+
+    @Override
+    public List<Article> findBybookname(String bookname) {
+        return articleRepository.findBybookname(bookname);
+    }
+
+    @Override
+    public List<Article> findByTitle(String title){
+        return articleRepository.findByTitle(title);
+    }
+
+    @Override
+    public List<Article> findByArticleAuthor(String articleAuthor){
+        return articleRepository.findByArticleAuthor(articleAuthor);
+    }
+
+}
---
+++ src/main/java/cn/edusdu/sdudoc/sdudocdao/mapper/UserMapper.java
@@ -0,0 +1,30 @@
+package cn.edusdu.sdudoc.sdudocdao.mapper;
+
+import cn.edu.sdu.sdudoc.sdudocdao.entity.User;
+import cn.edu.sdu.sdudoc.sdudocdao.entity.UserExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface UserMapper {
+    long countByExample(UserExample example);
+
+    int deleteByExample(UserExample example);
+
+    int deleteByPrimaryKey(String uid);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    List<User> selectByExample(UserExample example);
+
+    User selectByPrimaryKey(String uid);
+
+    int updateByExampleSelective(@Param("record") User record, @Param("example") UserExample example);
+
+    int updateByExample(@Param("record") User record, @Param("example") UserExample example);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
