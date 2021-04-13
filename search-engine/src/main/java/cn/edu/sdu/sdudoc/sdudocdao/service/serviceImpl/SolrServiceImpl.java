7b757ea5af930233580b71f153e9017826dff2f4
KazeDog <835778964@qq.com>
2021-04-13 23:43:48 +0800

    9

--- search-engine/src/main/java/cn/edu/sdu/sdudoc/sdudocdao/service/serviceImpl/SolrServiceImpl.java
+++ search-engine/src/main/java/cn/edu/sdu/sdudoc/sdudocdao/service/serviceImpl/SolrServiceImpl.java
@@ -23,7 +23,7 @@ public class SolrServiceImpl implements SolrService {
 
     @Override
     public JSONArray queryHead(String type, String keyword) throws SolrServerException, IOException {
-        System.out.println(type+":*"+keyword+"*");
+        System.out.println(type+":"+keyword);
         SolrQuery solrQuery=new SolrQuery();
         //查询的属性必须在solr库中有定义 ，否则会报错：
         //Error from server at http://192.168.65.128:8080/solr: undefined field gnames
@@ -31,7 +31,7 @@ public class SolrServiceImpl implements SolrService {
         //solrQuery.setQuery("gname:苹果手机");
         //针对多属性进行查询(注意多条件连接时，之间要有空格)
         //solrQuery.setQuery("gname:手机 || ginfo:苹果");
-        solrQuery.setQuery(type+":*"+keyword+"*");
+        solrQuery.setQuery(type+":"+keyword);
         QueryResponse queryResponse = solrClient.query(solrQuery);
         SolrDocumentList results = queryResponse.getResults();
         JSONArray jsonArray = new JSONArray();
