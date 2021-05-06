8b51bcf0b998756c8644c0feb06e225a225444fd
KazeDog <835778964@qq.com>
2021-05-07 00:05:15 +0800

    22

--- search-engine/src/main/java/cn/edu/sdu/sdudoc/sdudocmysql/SearchEngineApplication.java
+++ search-engine/src/main/java/cn/edu/sdu/sdudoc/sdudocmysql/SearchEngineApplication.java
@@ -3,16 +3,16 @@ package cn.edu.sdu.sdudoc.sdudocmysql;
 import javafx.application.Application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories(basePackages = "cn.edu.sdu.sdudoc.sdudocmbg.repository")
+//@EntityScan(basePackages = "cn.edu.sdu.sdudoc.sdudocmbg.entity")
 @SpringBootApplication
 public class SearchEngineApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SearchEngineApplication.class, args);
-    }
-
-    SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(Application.class);
     }
 }
