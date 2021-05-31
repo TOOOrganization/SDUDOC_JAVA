304e0c80d7aa9fafaab8d345b2caa5676c6be63f
iwscream <874223998>
2021-05-31 15:38:13 +0800

    add a table

---
+++ sdudoc-mbg/src/main/java/cn/edu/sdu/sdudoc/sdudocmbg/repository/ds2/SmsSearchHistoryRepository.java
@@ -0,0 +1,7 @@
+package cn.edu.sdu.sdudoc.sdudocmbg.repository.ds2;
+
+import cn.edu.sdu.sdudoc.sdudocmbg.entity.ds2.SmsSearchHistory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SmsSearchHistoryRepository extends JpaRepository<SmsSearchHistory, Integer> {
+}
