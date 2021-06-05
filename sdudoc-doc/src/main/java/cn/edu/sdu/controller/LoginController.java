c3265bdd5988829172f20ed4b09942285e660d0e
iwscream <874223998>
2021-06-06 00:47:19 +0800

    add doc login controller

--- sdudoc-doc/src/main/java/cn/edu/sdu/controller/LoginController.java
+++ sdudoc-doc/src/main/java/cn/edu/sdu/controller/LoginController.java
@@ -1,12 +1,13 @@
 package cn.edu.sdu.controller;
 
 import cn.edu.sdu.annonations.UserLoginToken;
+import cn.edu.sdu.api.CommonResult;
 import cn.edu.sdu.entity.ds1.UmsUser;
-import cn.edu.sdu.service.TokenService;
-import cn.edu.sdu.service.UmsUserService;
+import cn.edu.sdu.service.EditorUserDetailsService;
 import com.alibaba.fastjson.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Example;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
@@ -16,59 +17,39 @@ import java.util.Optional;
 public class LoginController {
 
     @Autowired
-    UmsUserService service;
-    @Autowired
-    TokenService tokenService;
+    EditorUserDetailsService userDetailsService;
 
     @PostMapping("/login")
-    public JSONObject login(String username, String password) {
-        JSONObject jsonObject = new JSONObject();
-        UmsUser user = new UmsUser();
-        user.setUsername(username);
-        user.setPassword(password);
-
-        Optional<UmsUser> userForBase = service.findOne(Example.of(user));
-
-        if (userForBase.isPresent()) {
-            if (!userForBase.get().getPassword().equals(user.getPassword())) {
-                jsonObject.put("message", "登录失败，密码错误");
-            } else {
-                String token = tokenService.getToken(userForBase.get());
-                jsonObject.put("token", token);
-                jsonObject.put("user", userForBase);
-            }
-        }else{
-            jsonObject.put("message", "登录失败，用户不存在或密码错误");
-        }
-
-        return jsonObject;
+    public CommonResult<String> login(String username, String password) {
+        return userDetailsService.login(username, password);
     }
 
     @PostMapping("/register")
-    public Object register(String username, String password, String nickname) {
-        JSONObject jsonObject = new JSONObject();
-        UmsUser fuser = new UmsUser();
-        fuser.setUsername(username);
-        Optional<UmsUser> one = service.findOne(Example.of(fuser));
-
-        if (one.isPresent()) {
-            jsonObject.put("message", "用户已存在");
-        } else {
-            fuser.setPassword(password);
-            fuser.setNickname(nickname);
-            fuser.setStatus(1);
-            fuser.setCount(1);
-
-            service.save(fuser);
-            jsonObject = login(username, password);
-        }
-
-        return jsonObject;
+    public CommonResult<String> register(String username, String password, String nickname) {
+        return userDetailsService.register(username, password, nickname);
     }
 
-    @UserLoginToken
-    @GetMapping("/get_message")
-    public String getMessage() {
-        return "通过验证";
+    @PreAuthorize("hasAnyRole('author')")
+    @PostMapping("/set_password")
+    public CommonResult<String> setPassword(String username, String password, String confirmPassword) {
+        return userDetailsService.setPassword(username, password, confirmPassword);
+    }
+
+    @PreAuthorize("hasAnyRole('author')")
+    @PostMapping("/get_by_username")
+    public CommonResult<UmsUser> getByUsername(String username) {
+        return userDetailsService.getByUsername(username);
+    }
+
+    @PreAuthorize("hasAnyRole('author')")
+    @PostMapping("/get_by_id")
+    public CommonResult<UmsUser> getById(Integer id) {
+        return userDetailsService.getById(id);
+    }
+
+    @PreAuthorize("hasAnyRole('author')")
+    @PostMapping("/update_user_info")
+    public CommonResult<UmsUser> updateUserInfo(@RequestBody UmsUser user) {
+        return userDetailsService.updateUserInfo(user);
     }
 }
--- sdudoc-doc/src/test/java/cn/edu/sdu/controller/LoginTest.java
+++ sdudoc-doc/src/test/java/cn/edu/sdu/controller/LoginTest.java
@@ -17,37 +17,8 @@ public class LoginTest {
     UmsUserRepository repository;
 
     @Test
-    public void getLogin() {
-        Optional<UmsUser> user = repository.findById(1);
-        if (user.isPresent()) {
-            JSONObject result = (JSONObject) login.login(user.get().getUsername(), user.get().getPassword());
-            System.out.println(result.get("token"));
-            System.out.println(result);
-        }else {
-            System.out.println("sb");
-        }
-//        UmsUser user = new UmsUser();
-//        user.setUid("123");
-//        user.setUsername("222");
-//        user.setPassword("222");
-//        user.setStatus(0);
-//        user.setCount(0);
-//        JSONObject result = (JSONObject) login.login(user);
-//        System.out.println(result);
-//        System.out.println(result.get("token"));
-//        System.out.println(login.getMessage());
-
-//        System.out.println(LoginController.login(user.get()));
-//        System.out.println(hello.hello());
-    }
+    public void getLogin() {}
 
     @Test
-    public void register() {
-        String username = "123";
-        String password = "123";
-        String nickname = "123";
-
-        JSONObject object = (JSONObject) login.register(username, password, nickname);
-        System.out.println(object);
-    }
+    public void register() {}
 }
